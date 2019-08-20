85083aa65503980989e83a4d81fa79309e025347
danielmoneyloop <daniel.latimer@moneyloop.com.au>
2019-08-20 14:46:03 +1000

    precompile change?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # For local precomilation
-  config.assets.prefix = "/dev-assets"
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
