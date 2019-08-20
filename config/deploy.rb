57618ac17e06f86b1906f863083920390b73683c
danielmoneyloop <daniel.latimer@moneyloop.com.au>
2019-08-20 12:12:48 +1000

    updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
-server '165.22.55.23', port: 22, roles: [:web, :app, :db], primary: true
+server '167.71.250.93', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:moneyloop-team/moneyloop_website.git'
 set :application,     'moneyloop'
-set :user,            'dan'
+set :user,            'rails'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :branch, ENV['BRANCH'] if ENV['BRANCH']
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/moneyloop.pem) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
